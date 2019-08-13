5baa538d68d11c909bc2fc5a80f1e320f5783096
Iheartdog <54084479+Iheartdog@users.noreply.github.com>
2019-08-14 11:07:05 +1200

    Created the main routine

---
+++ RPG Sheet Tracker.py
@@ -0,0 +1,11 @@
+"""
+RPG Sheet Tracker.py
+Author: Rachel Given
+Date Created: 14/08/2019
+Last Edited: 15/08/2019
+Make a system to keep track of RGP player stats
+"""
+
+
+if __name__ == "__main__":
+    print("Hey")
